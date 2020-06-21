9f9af3d7262e3d6476baf5c96c1c7e74d1fd1e10
Thinh Le <thinhlefrs@gmail.com>
2020-06-21 14:36:09 +0700

    Initial Commit

---
+++ CircularMeterView/CircularMeterView.h
@@ -0,0 +1,19 @@
+//
+//  CircularMeterView.h
+//  CircularMeterView
+//
+//  Created by Thinh Le on 6/21/20.
+//  Copyright © 2020 Thinh Le. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for CircularMeterView.
+FOUNDATION_EXPORT double CircularMeterViewVersionNumber;
+
+//! Project version string for CircularMeterView.
+FOUNDATION_EXPORT const unsigned char CircularMeterViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <CircularMeterView/PublicHeader.h>
+
+
